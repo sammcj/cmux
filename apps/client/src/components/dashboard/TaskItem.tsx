53cd262c0199e08a657933fab46bdb521b49e92d
Austin Wang <38676809+austinywang@users.noreply.github.com>
2025-11-11 21:41:26 +0000

    ok

--- apps/client/src/components/dashboard/TaskItem.tsx
+++ apps/client/src/components/dashboard/TaskItem.tsx
@@ -191,7 +191,7 @@ export const TaskItem = memo(function TaskItem({
           <div
             className={clsx(
               "relative grid w-full items-center py-2 pr-3 cursor-default select-none",
-              "grid-cols-[24px_36px_1fr_120px_58px]",
+              "grid-cols-[24px_36px_1fr_120px_70px]",
               isOptimisticUpdate
                 ? "bg-white/50 dark:bg-neutral-900/30 animate-pulse"
                 : "bg-white dark:bg-neutral-900/50 hover:bg-neutral-50/90 dark:hover:bg-neutral-600/60",
