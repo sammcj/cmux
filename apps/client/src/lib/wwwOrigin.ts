88c97370f046f985c32ea2312f16121583854274
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-12-04 00:08:10 -0800

    wip

--- apps/client/src/lib/wwwOrigin.ts
+++ apps/client/src/lib/wwwOrigin.ts
@@ -2,6 +2,7 @@ import { normalizeOrigin } from "@cmux/shared";
 import { env } from "@/client-env";
 
 export const WWW_ORIGIN = normalizeOrigin(
-  process.env.NEXT_PUBLIC_RELATED_WWW_ORIGIN_PREVIEW ||
-    env.NEXT_PUBLIC_WWW_ORIGIN
+  // TODO: handle main to never use this
+  // process.env.NEXT_PUBLIC_RELATED_WWW_ORIGIN_PREVIEW ||
+  env.NEXT_PUBLIC_WWW_ORIGIN
 );
